63879b3c225173a2d18c40377d23f31bda76adce
Norgius <dmitry.schadrin21@yandex.ru>
2023-06-09 23:24:29 +0300

    Add script for user registration

--- connecting_to_chat.py
+++ connecting_to_chat.py
@@ -5,6 +5,7 @@ import logging
 import json
 from textwrap import dedent
 
+import aiofiles
 from environs import Env
 
 logger = logging.getLogger(__name__)
@@ -14,17 +15,41 @@ async def log_in():
     pass
 
 
+async def register():
+    pass
+
+
 async def connect_to_chat(host: str, port: int, chat_user_token: str):
     reader, writer = await asyncio.open_connection(host, port)
     try:
-        response_in_bytes = await reader.read(1024)
+        response_in_bytes = await reader.readline()
         response = response_in_bytes.decode('utf-8')
         logger.debug(f'sender: {response}')
+        if not chat_user_token:
+            message = '\n'
+            logger.debug(f'user: {message}')
+            writer.write(message.encode('utf-8'))
+            response_in_bytes = await reader.readline()
+            response = response_in_bytes.decode("utf-8")
+            logger.debug(f'sender: {response}')
+            print(response)
+            nickname = input('Введите ваш будущий никнейм: ')
+            logger.debug(f'user: {nickname}')
+            writer.write(f'{nickname}\r\n'.encode('utf-8'))
+            response_in_bytes = await reader.readline()
+            response = response_in_bytes.decode("utf-8")
+            response = json.loads(response)
+            async with aiofiles.open('.env', 'a') as file:
+                line = f'CHAT_USER_TOKEN={response.get("account_hash")}\n'
+                await file.write(line)
+            sys.stdout.write('Повторно запустите скрипт для отправки сообщений')
+            exit(0)
+
         message = f'{chat_user_token}\r\n'
         logger.debug(f'user: {message}')
         writer.write(message.encode('utf-8'))
         await writer.drain()
-        response_in_bytes = await reader.read(1024)
+        response_in_bytes = await reader.readline()
         response = response_in_bytes.decode("utf-8")
         logger.debug(f'sender: {response}')
         if response == '\n':
@@ -33,7 +58,9 @@ async def connect_to_chat(host: str, port: int, chat_user_token: str):
             Проверьте его или зарегистрируйте заново.
             '''))
         else:
-            message = '3-я попытка\r\n\n'
+            message = input('Введите сообщение: ')
+            # message = sys.stdin.readline()
+            message = f'{message}\r\n\n'
             logger.debug(f'user: {message}')
             writer.write(message.encode('utf-8'))
             await writer.drain()
@@ -48,7 +75,7 @@ def main():
     env.read_env()
     host_for_chat = env.str('HOST_FOR_CHAT', '')
     port_for_chat = env.int('PORT_FOR_AUTH_CHAT', 0)
-    chat_user_token = env.str('CHAT_USER_TOKEN')
+    chat_user_token = env.str('CHAT_USER_TOKEN', '')
     logging.basicConfig(
         filename='connecting_to_chat.log',
         filemode='w',
